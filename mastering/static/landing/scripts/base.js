09d1b5bf717a0e33d473f8d362d6ad6639ec62b3
Avaaren <avaaren@mail.ru>
2020-02-28 14:11:35 +0300

    Reworked navbar css and js. Navbar behavior now with jQuery

--- mastering/static/landing/scripts/base.js
+++ mastering/static/landing/scripts/base.js
@@ -1,28 +1,3 @@
-let el = document.querySelectorAll('.navbar_ul_item, .navbar_sub_ul_item');
-    for (let i = 0; i < el.length; i++) {
-      el[i].addEventListener('mouseenter', showMenu, false);
-      el[i].addEventListener('mouseleave', hideMenu, false);
-    }
-    function showMenu() {
-      if (this.children.length > 1) {
-        this.children[1].style.height = 'auto';
-        this.children[1].style.opacity = '1';
-        this.children[1].style.overflow = 'visible';
-      }
-      else {
-        return false
-      }
-    }
-    function hideMenu() {
-      if (this.children.length > 1) {
-        this.children[1].style.height = '0';
-        this.children[1].style.opacity = '0';
-        this.children[1].style.overflow = 'hidden';
-      }
-      else {
-        return false
-      }
-    }
 
 $(function(){
   $(window).scroll(function(){
@@ -32,5 +7,26 @@ $(function(){
     else{
       $('.sidebar').hide(200);
     }
-  })
+  });
+
+  $('.navbar_ul_item').hover(
+    function(){
+      $(this).find('ul.navbar_sub_ul').show(200);
+
+    },  
+    function(){
+      $(this).find('ul.navbar_sub_ul').hide(300);
+    } 
+  );
+
+  $('.navbar_sub_ul_item').hover(
+    function(){
+      $(this).find('ul.navbar_second_level_sub_ul').show(200);
+      
+    },  
+    function(){
+      $(this).find('ul.navbar_second_level_sub_ul').hide(300);
+    } 
+  );  
+  
 });
